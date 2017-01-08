8cd16284c24c310d31a158a85d28ad540f509e25
Stephen Frizell <steve.frizell@gmail.com>
2017-01-08 02:51:25 -0500

    new class EmailNotifyParametersReader to obtain EmailNotifyParameters entities from the database.

--- src/Pennsouth/MdsBundle/Service/EmailNotifyParametersReader.php
+++ src/Pennsouth/MdsBundle/Service/EmailNotifyParametersReader.php
@@ -8,8 +8,48 @@
 
 namespace Pennsouth\MdsBundle\Service;
 
+use Doctrine\DBAL\Query\QueryException;
+use Doctrine\ORM\EntityManager;
 
 class EmailNotifyParametersReader
 {
+
+    private $entityManager;
+
+
+    public function __construct (EntityManager $entityManager ) {
+
+        $this->entityManager = $entityManager;
+
+    }
+
+    public function getEntityManager() {
+        return $this->entityManager;
+    }
+
+    public function getEmailNotifyParameters($reportOrProcessName) {
+
+        try {
+            $query = $this->getentityManager()->createQuery(
+                'Select enp 
+              from PennsouthMdsBundle:EmailNotifyParameters enp 
+              where enp.reportOrProcessName =:reportOrProcessName'
+            )->setParameter('reportOrProcessName', $reportOrProcessName)
+            ;
+            $emailNotifyParametersRows = $query->getResult();
+
+            return $emailNotifyParametersRows;
+        }
+        catch (QueryException $exception) {
+            print("\n QueryException in EmailNotifyParametersReader->getEmailNotifyParameters(): exception->getMessage(): " . $exception->getMessage() . "\n");
+            throw $exception;
+        }
+        catch (\Exception $exception) {
+            print("\n Exception in EmailNotifyParametersReader->getEmailNotifyParameters(): exception->getMessage(): " . $exception->getMessage() . "\n");
+            throw $exception;
+        }
+
+
+    }
 
 }
